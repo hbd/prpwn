adcf341f78d9f06e408919ad92e5baca10d95785
Zachary A. Keener <zakkeener@gmail.com>
2019-02-09 00:01:05 -0500

    Cleanup and delete button on bitbucket

---
+++ src/bitbucket-idle.js
@@ -0,0 +1,8 @@
+deleteButton();
+
+function deleteButton()
+{
+    console.log("Deleting the PR button >:)");
+    document.getElementById("fulfill-pullrequest").remove();
+    console.log("Double Pwned");
+}
--- src/bitbucket-start.js
+++ src/bitbucket-start.js
@@ -1,11 +1,3 @@
-/* let textContent = `
- *     #fulfill-pullrequest {
- * 	display: none !important;
- *     }
- * `;
- * 
- * chrome.tabs.insertCSS(tab.id, {code:textContent, allFrames:false, runAt:"document_start"});
- * */
 hideButton();
 
 function hideButton()
@@ -14,13 +6,12 @@ function hideButton()
 
     let thing = document.documentElement;
     const style = document.createElement('style');
-
     style.textContent = `
     #fulfill-pullrequest {
 	display: none !important;
     }
     `;
     thing.appendChild(style);
-    /* document.getElementById("fulfill-pullrequest").style.display = "none";*/
+
     console.log("Pwned");
 }
